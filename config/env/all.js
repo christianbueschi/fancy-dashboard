865eb2a9bdca3b52d33215659cca1c7de0793c90
cbueschi <christian.bueschi@namics.com>
2015-04-21 16:57:06 +0200

    init

---
+++ config/env/all.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'MEANJS - Angular Material - LESS',
+		description: 'MEANJS Fork with integration of Angular Material and LESS support',
+		keywords: 'MongoDB, Express, AngularJS, Node.js'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/angular-material/angular-material.min.css'
+			],
+			js: [
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js', 
+				'public/lib/angular-cookies/angular-cookies.js', 
+				'public/lib/angular-animate/angular-animate.js', 
+				'public/lib/angular-touch/angular-touch.js', 
+				'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-aria/angular-aria.min.js',
+				'public/lib/angular-material/angular-material.min.js'
+			]
+		},
+		css: [
+			'public/dist/application.css'
+		],
+		js: [
+			'public/config.js',
+			'public/application.js',
+			'public/modules/*/*.js',
+			'public/modules/*/*[!tests]*/*.js'
+		],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
---
+++ public/application.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Start by defining the main module and adding the module dependencies
+angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+
+// Setting HTML5 Location Mode
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
+	function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+	}
+]);
+
+// Setting Angular Material and Theme
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$mdThemingProvider',
+	function($mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('green')
+			.accentPalette('lime')
+	}
+]);
+
+//Then define the init function for starting up the application
+angular.element(document).ready(function() {
+	//Fixing facebook bug with redirect
+	if (window.location.hash === '#_=_') window.location.hash = '#!';
+
+	//Then init the app
+	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+});
